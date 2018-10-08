b967a0b35a0d6228d5e84959863c6ecf6f934aec
Jon Carrasco <jcarrascobar@gmail.com>
2018-10-08 18:57:15 +0200

    commit final 6

--- Metodos/Metodos/Program.cs
+++ Metodos/Metodos/Program.cs
@@ -196,7 +196,7 @@ namespace Metodos
         //    Console.WriteLine("introduzca el numero minimo del rango");
         //    min = Convert.ToInt32(Console.ReadLine());
         //    Generador(num, max, min);
-            
+
 
         //    Console.ReadLine();
         //}
@@ -220,18 +220,184 @@ namespace Metodos
         //    int num;
         //    Console.WriteLine("introduzca un numero");
         //    num = Convert.ToInt32(Console.ReadLine());
-        //    int resultado = Factorial(num);
+        //    int resultado = SacarFactor(num);
         //    Console.WriteLine(" el factorial del numero es: " + resultado);
         //    Console.ReadLine();
         //}
-        //public static int Factorial(int numa)
+        //public static int SacarFactor(int numa)
         //{
-        //    for (int i = 0; i < numa; i++)
+        //    int producto = 1;
+        //    for (int i = numa; i > 0; i--)
         //    {
-        //        int producto = 1;
-        //        return producto = producto * numa;
+        //    producto = producto * i;
         //    }
+        //    return producto;
         //}
 
-    }
+
+        //Ejercicio 5:
+        //Crea una aplicación que nos cuente el número de cifras de un número entero positivo (hay que controlarlo) pedido por teclado.
+        //Crea un método que realice esta acción, pasando el número por parámetro, devolverá el número de cifras.
+
+        //static void Main(string[] args)
+        //{
+        //    int number;
+        //    Console.WriteLine("introduce un numero entero");
+        //    number = Convert.ToInt32(Console.ReadLine());
+        //    if (number >= 0)
+        //    {
+        //        int sumaDig = CuentaCif(number);
+        //        Console.WriteLine("La suma de los digitos es: " + sumaDig);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Numero incorrecto, introduzca otro");
+        //    }
+        //    Console.ReadLine();
+        //}
+        //public static int CuentaCif(int digit)
+        //{
+
+        //}
+
+        //Ejercicio 6:
+        //Crea un aplicación que nos convierta una cantidad de euros introducida por teclado a otra moneda, estas pueden ser a dolares,
+        //yenes o libras. El método tendrá como parámetros, la cantidad de euros y la moneda a pasar que sera una cadena,
+        //este no devolverá ningún valor, mostrara un mensaje indicando el cambio (void).
+        //El cambio de divisas son:
+        //0.86 libras es un 1 €
+        //1.28611 $ es un 1 €
+        //129.852 yenes es un 1 €
+
+        //static void Main(string[] args)
+        //{
+        //Console.Write("introduzca la cantidad de Euros a convertir");
+        //int coin = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("introduzca la moneda a convertir");
+        //int coinValue = Transfer(
+        //}
+
+
+        //Ejercicio 9:
+        //Método es_perfecto que determine si un número es o no perfecto. A este método se le pasa un número y devuelve: 
+        //Verdadero: si el número es perfecto
+        //Falso: si no es perfecto
+        //Un número es perfecto si la suma de sus divisores excepto él, es igual al propio número.Ejemplo: 
+        //6=1+2+3 es perfecto
+        //Utilízalo en  un programa que pida  por teclado  números hasta que se teclee un  0  e indique  los siguientes resultados: 
+        //Cantidad de números pares
+        //Cantidad de números perfectos
+        //Cantidad de múltiplos de 5
+
+
+        //Ejercicio 10:
+        //Diseña  un  método  pedirMes().  Solicita  un  número  de  mes  y  valida  que  se  trata  de  un número de mes correcto.
+        //Devuelve dicho número de mes. 
+        //Diseña un método pedirAnio(). Recibe como parámetros los límites entre los que debe de estar el año que se debe solicitar
+        //por teclado. Devuelve el año entre dichos límites.
+        //Diseña un método pedirDia(). Solicita un número de día por teclado, y dado un mes y un año válidos 
+        //recibidos por  parámetro, devuelve un  día correcto.  (Nota:  puede llamar  al método esBisiesto)
+        //Crea un nuevo método mostrarSiguienteDia() que recibe un día, un mes y un año válido (estos tres datos forman la fecha actual) 
+        //y muestra en pantalla 2 fechas: la actual y la del día siguiente, a calcular a partir del día actual.
+        //Pruébalo en  un programa  que pida  al usuario  una fecha(pedirDia(), pedirMes(), pedirAnio()) 
+        //y muestre la fecha del día siguiente.
+        //Ejemplo:  
+        //Fecha actual: 31/12/2011 
+        //Día siguiente: 1/1/2012 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduzca numero de mes");
+            int mes = Convert.ToInt32(Console.ReadLine());
+            int valorMes = PedirMes(mes);
+            Console.WriteLine("El mes es el " + valorMes);
+            Console.WriteLine("introduzca año limite inferior");
+            int annoMin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca año limite superior");
+            int annoMax = Convert.ToInt32(Console.ReadLine());
+
+            if (annoMin>annoMax)
+            {
+                Console.WriteLine("Error, su limite minimo es superior al maximo, se le modifica al modo correcto");
+                int cambio = annoMin;
+                annoMin = annoMax;
+                annoMax = cambio;
+            }
+            int Anno = PedirAnno(annoMin,annoMax);
+            Console.WriteLine("Su año es: " + Anno);
+
+            Console.WriteLine("Introduzca el numero del día");
+            int dia = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            int diaCorrecto = PedirDia(dia, valorMes, Anno);
+            Console.WriteLine("Fecha Actual es: " + diaCorrecto + "/" + valorMes + "/" + Anno);
+
+            //Introducir el Array
+            int[] mesesArr = PedirArr(diaCorrecto, valorMes, Anno);
+
+            if (diaCorrecto == mesesArr[valorMes-1])
+            {
+                Console.WriteLine("Fecha del dia siguiente es: 01" + "/" + valorMes + "/" + Anno);
+            }
+            else
+            {
+                Console.WriteLine("Fecha dia siguiente es: " + (diaCorrecto + 1) + "/" + valorMes + "/" + Anno);
+            }
+
+            Console.ReadLine();
+        }
+        public static int PedirMes (int introMes)
+        {
+            while (introMes<=0 || introMes>12)
+            {
+                Console.WriteLine("Error, inserte un numero de mes correcto");
+                introMes = Convert.ToInt32(Console.ReadLine());
+            }
+            return introMes;
+        }
+        public static int PedirAnno (int minAnno,int maxAnno)
+        {
+            Random rndm = new Random();
+            int annoRndm = rndm.Next(minAnno, maxAnno + 1);
+            return annoRndm;
+        }
+        public static int PedirDia (int introDia, int introMes, int introAnno)
+        {
+            bool leapYear = DateTime.IsLeapYear(introAnno);
+            int[] mesesArr = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; //La posición del mes es 1 menos que la del mes real
+
+            if (leapYear)
+            {
+                mesesArr[1] = 29;
+            }
+
+            while (introDia < mesesArr[introMes -1] || introMes <=0)
+            {
+                Console.WriteLine("Su día no corresponde a un día real del mes definido: " + intromes);
+                Console.WriteLine("Inserte u nuevo día correcto, por favor");
+                introDia = Convert.ToInt32(Console.ReadLine());
+            }
+            return mesesArr[12];
+
+        }
+        public static int PedirArr(int introDia, int introMes, int introAnno)
+        {
+            bool leapYear = DateTime.IsLeapYear(introAnno);
+            int[] mesesArr = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; //La posición del mes es 1 menos que la del mes real
+
+            if (leapYear)
+            {
+                mesesArr[1] = 29;
+            }
+
+            while (introDia < mesesArr[introMes - 1] || introMes <= 0)
+            {
+                Console.WriteLine("Su día no corresponde a un día real del mes definido: " + intromes);
+                Console.WriteLine("Inserte u nuevo día correcto, por favor");
+                introDia = Convert.ToInt32(Console.ReadLine());
+            }
+            return mesesArr[12];
+        }
 }
